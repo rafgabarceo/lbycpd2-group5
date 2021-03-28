268438b5a9e3b8eb922d3e73120efbcda31dbb6a
Rafael Gabriel Arceo <rafael_gabriel_arceo@dlsu.edu.ph>
2021-03-28 20:05:50 +0800

    Initial commit

---
+++ src/main/java/com/lbycpd2/todoexp/TodoexpApplication.java
@@ -0,0 +1,13 @@
+package com.lbycpd2.todoexp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoexpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TodoexpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lbycpd2/todoexp/TodoexpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lbycpd2.todoexp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoexpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
