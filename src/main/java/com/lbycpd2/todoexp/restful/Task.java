2505ed4b9004c646f68e63df81011102059fc404
Rafael Gabriel Arceo <rafael_gabriel_arceo@dlsu.edu.ph>
2021-04-07 20:38:06 +0800

    Re-structured back-end architecture. Created new Task class that is not part of the database. This is what ParentTask and ChildTask inherit.

---
+++ src/main/java/com/lbycpd2/todoexp/restful/Task.java
@@ -0,0 +1,43 @@
+package com.lbycpd2.todoexp.restful;
+
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass
+public class Task {
+    private String title;
+    private String description;
+    private boolean status;
+
+    public Task() {
+    }
+
+    public Task(String title, String description, boolean status) {
+        this.title = title;
+        this.description = description;
+        this.status = status;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+}
