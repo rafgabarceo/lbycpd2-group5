733cec2c5550fa59d0c68761c9748524a2b19870
Rafael Gabriel Arceo <rafael_gabriel_arceo@dlsu.edu.ph>
2021-04-08 23:05:02 +0800

    Created necessary services and controllers

---
+++ src/main/java/com/lbycpd2/todoexp/restful/userpackage/UserService.java
@@ -0,0 +1,62 @@
+package com.lbycpd2.todoexp.restful.userpackage;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+
+import javax.transaction.Transactional;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @GetMapping
+    public List<User> getUsers() {
+        return userRepository.findAll();
+    }
+
+    @GetMapping
+    public User getUser(Long userId){
+        return userRepository.getOne(userId);
+    }
+
+    public void addUser(User newUser){
+        Optional<User> userOptional = userRepository.findUserByEmail(newUser.getEmail());
+        if(userOptional.isPresent()) {
+            throw new IllegalStateException("Email is already taken.");
+        }
+        userRepository.save(newUser);
+    }
+
+    public void deleteUser(Long userId){
+        boolean exists = userRepository.existsById(userId);
+        if(!exists) throw new IllegalStateException("User with id " + userId + " does not exist.");
+        userRepository.deleteById(userId);
+    }
+
+    @Transactional
+    public void updateUser(Long userId, String username, String password, String email, Double experience){
+        boolean exists = userRepository.existsById(userId);
+        if(!exists) throw new IllegalStateException("User " + username + " with user id " + userId + " does not exist.");
+
+        User userUpdate = userRepository.getOne(userId);
+
+        if(username != null && username.length() > 0 && !username.equals(userUpdate.getUsername())) userUpdate.setUsername(username);
+        if(email != null && email.length() > 0 && !email.equals(userUpdate.getEmail())){
+            if(userRepository.findUserByEmail(email).isPresent()) {
+                throw new IllegalStateException("Email already in database.");
+            }
+            userUpdate.setEmail(email);
+        }
+        if(password != null && !password.equals(userUpdate.getPassword())) userUpdate.setPassword(password);
+        if(experience > 0) userUpdate.setExperience(userUpdate.getExperience() + experience);
+    }
+}
