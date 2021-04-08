1be91abd6eb3b0044f96747354ba01018316b0e1
Rafael Gabriel Arceo <rafael_gabriel_arceo@dlsu.edu.ph>
2021-04-08 23:05:57 +0800

    Added new method to UserRepository.java to find user by email

--- src/main/java/com/lbycpd2/todoexp/restful/userpackage/UserRepository.java
+++ src/main/java/com/lbycpd2/todoexp/restful/userpackage/UserRepository.java
@@ -2,5 +2,9 @@ package com.lbycpd2.todoexp.restful.userpackage;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findUserByEmail(String email);
 }
