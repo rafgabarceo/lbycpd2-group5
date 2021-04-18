2c88a57c27d18b72e79523186af4912b35f61b41
Rafael Gabriel Arceo <rafael_gabriel_arceo@dlsu.edu.ph>
2021-04-18 21:56:55 +0800

    Moved over files from previous project to make login screen work.

---
+++ src/main/java/com/lbycpd2/todoexp/restful/email/EmailSender.java
@@ -0,0 +1,5 @@
+package com.lbycpd2.todoexp.restful.email;
+
+public interface EmailSender {
+    void send(String to, String email);
+}
---
+++ src/main/java/com/lbycpd2/todoexp/restful/registration/token/ConfirmationTokenRepository.java
@@ -0,0 +1,12 @@
+package com.lbycpd2.todoexp.restful.registration.token;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface ConfirmationTokenRepository extends JpaRepository<ConfirmationToken, Long> {
+
+    Optional<ConfirmationToken> findByToken(String token);
+}
