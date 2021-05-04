cf4a09aea802577eb09db8f3dc5b8f194fd40309
Rafael Gabriel Arceo <rafael_gabriel_arceo@dlsu.edu.ph>
2021-05-04 21:35:19 +0800

    Fixed authentication per user through principal

--- src/main/java/com/lbycpd2/todoexp/restful/security/UserSecurity.java
+++ src/main/java/com/lbycpd2/todoexp/restful/security/UserSecurity.java
@@ -3,7 +3,6 @@ package com.lbycpd2.todoexp.restful.security;
 import com.lbycpd2.todoexp.restful.user.User;
 import com.lbycpd2.todoexp.restful.user.UserService;
 import lombok.AllArgsConstructor;
-import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
 
 import java.security.Principal;
@@ -14,7 +13,6 @@ public class UserSecurity {
     private final UserService userService;
     public boolean hasUserId(Principal principal, String id){
         User user = userService.getUser(id);
-        System.out.println(principal.getName());
         return principal.getName().equals(user.getEmail());
     }
 }
